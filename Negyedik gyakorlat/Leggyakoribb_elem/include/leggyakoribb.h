b2f306b7d2f1e1f758124e65dbe66c94b964a354
SzSoma1 <gemailsoma@gmail.com>
2024-03-31 12:47:59 +0200

    Negyedik_gyakorlat

---
+++ Negyedik gyakorlat/Leggyakoribb_elem/include/leggyakoribb.h
@@ -0,0 +1,9 @@
+#ifndef LEGGYAKORIBB_H
+#define LEGGYAKORIBB_H
+
+/**
+ * Legygyakrabban előforduló szám darabszámát adja vissza
+ */
+int mostCommonElementOccurrence(int array[], int size);
+
+#endif /* LEGGYAKORIBB_H */
